e07d1a11f8f8a6ee952aebfec9f7988db83e86e6
Sergi Roca Laguna <sergirocalaguna@Sergis-MacBook-Pro.local>
2025-05-03 20:38:55 +0200

    audit exercise

--- solana-audit/programs/solana-audit/src/lib.rs
+++ solana-audit/programs/solana-audit/src/lib.rs
@@ -25,7 +25,7 @@ pub struct Slashing<'info> {
     pub stake_pool: AccountLoader<'info, StakePool>,
     pub token_program: Program<'info, Token>,
 }
- //@audit why are is_locker arbitrary? and what is the purpose of router?
+ //@audit why is is_locker arbitrary? and what is the purpose of router?
 pub fn slashing_handler<'info>(
     ctx: Context<Slashing>,
     amount: u64,
@@ -35,19 +35,25 @@ pub fn slashing_handler<'info>(
     {    
         let stake_pool = &mut ctx.accounts.stake_pool.load_mut()?;
         let pool = &mut stake_pool.reward_pools[usize::from(router)];
+        //@audit is_locked can be forced arbitrarily, could affect the staker's reward
         pool.is_locked = is_locked;
+        //    /** Minimum duration for lockup. At this point, the staker would receive the base weight. In seconds. */
+        //    /** Maximum duration for lockup. At this point, the staker would receive the max weight. In seconds. */
+
 
         let cpi_ctx = CpiContext {
             program: ctx.accounts.token_program.to_account_info(),
+            //@audit vault is transferring to itself
             accounts: Transfer {
                 from: ctx.accounts.vault.to_account_info(),
                 to: ctx.accounts.vault.to_account_info(),
                 authority: ctx.accounts.stake_pool.to_account_info(),
             },
             remaining_accounts: Vec::new(),
-            //@audit is the seed public?
+            //@audit seeds made public
             signer_seeds: &[stake_pool_signer_seeds!(stake_pool)],
         };
+        //@audit user can transfer any amount of tokens
         let _ = token::transfer(cpi_ctx, amount);
 
         Ok(())
